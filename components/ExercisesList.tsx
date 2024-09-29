f8839f402b6656c0ae3ac7431f49fc13778230c6
qleml <cchristoph@student.ethz.ch>
2024-09-29 08:51:12 +0200

    empty exercises list before exp

---
+++ components/ExercisesList.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { ThemedText } from '@/components/ThemedText';
+
+
+const ExercisesList = () => {
+    return (
+        <View>
+            <Text className="dark:text-spartan-red-200">ExercisesList</Text>
+        </View>
+    );
+}
+
+export default ExercisesList;
